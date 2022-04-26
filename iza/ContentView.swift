1fa2269e4cc9aa24760ecc26de22654352b800c6
Pavel Kratochvil <xkrato61@vutbr.cz>
2022-04-26 12:11:36 +0200

    Initial Commit

---
+++ iza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iza
+//
+//  Created by Pavel Kratochvil on 26.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iza/izaApp.swift
@@ -0,0 +1,17 @@
+//
+//  izaApp.swift
+//  iza
+//
+//  Created by Pavel Kratochvil on 26.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct izaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
